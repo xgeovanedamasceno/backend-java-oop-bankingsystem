a1ca19869a6857d8c5ffa217c97bc0f7a38a39f0
xgeovanedamasceno <xgeovanedamasceno@gmail.com>
2022-05-22 16:13:00 -0300

    refactor: remove interface implements
    This commit removes the implementation of  'iconta' interface to decrease
    the coupling.

--- src/br/com/xgeovanedamasceno/bankingsystem/domain/Conta.java
+++ src/br/com/xgeovanedamasceno/bankingsystem/domain/Conta.java
@@ -1,8 +1,5 @@
 package br.com.xgeovanedamasceno.bankingsystem.domain;
-
-import br.com.xgeovanedamasceno.bankingsystem.application.IConta;
-
-public abstract class Conta implements IConta {
+public abstract class Conta {
 	
 	private static final int AGENCIA_PADRAO = 1;
 	private static int SEQUENCIAL = 1;
@@ -18,22 +15,6 @@ public abstract class Conta implements IConta {
 		this.cliente = cliente;
 	}
 
-	@Override
-	public void sacar(double valor) {
-		saldo -= valor;
-	}
-
-	@Override
-	public void depositar(double valor) {
-		saldo += valor;
-	}
-
-	@Override
-	public void transferir(double valor, IConta contaDestino) {
-		this.sacar(valor);
-		contaDestino.depositar(valor);
-	}
-
 	public int getAgencia() {
 		return agencia;
 	}
@@ -44,12 +25,5 @@ public abstract class Conta implements IConta {
 
 	public double getSaldo() {
 		return saldo;
-	}
-
-	protected void imprimirInfosComuns() {
-		System.out.println(String.format("Titular: %s", this.cliente.getNome()));
-		System.out.println(String.format("Agencia: %d", this.agencia));
-		System.out.println(String.format("Numero: %d", this.numero));
-		System.out.println(String.format("Saldo: %.2f", this.saldo));
 	}
 }
